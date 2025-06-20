b83bc60d2d2d0aaa2a1743db6b53db03c3150489
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-06-20 21:33:25 +0200

    improve monlist command

--- src/monlist.rs
+++ src/monlist.rs
@@ -19,18 +19,17 @@ impl MonlistRequestStatus {
 }
 
 pub fn init(state: &mut ScanState) {
-    let mut msg = NtpdPrivatePacket::empty();
-    msg.version = 2;
-    msg.implementation = packets::private::IMPL_XNTPD;
-    msg.reqcode = crate::packets::private::REQ_MON_GETLIST;
-    state.queue.push_back(AnyNTPPacket::Private(msg));
-
-    /* TODO
-     * ntpdc will actually attempt multiple types of requests to get an actual response
-     * because the interface has changed,
-     * we need to do the same
-     */
-
+    let impl_codes = [packets::private::IMPL_XNTPD, packets::private::IMPL_XNTPD_OLD];
+    let reqcodes = [packets::private::REQ_MON_GETLIST, packets::private::REQ_MON_GETLIST_1];
+    for impl_code in impl_codes {
+        for reqcode in reqcodes {
+            let mut msg = NtpdPrivatePacket::empty();
+            msg.version = 2;
+            msg.implementation = impl_code;
+            msg.reqcode = reqcode;
+            state.queue.push_back(AnyNTPPacket::Private(msg));
+        }
+    }
 }
 
 pub fn receive(state: &mut ScanState, pkt: &AnyNTPPacket) -> ScanTypeStatus {
@@ -62,13 +61,7 @@ pub fn receive(state: &mut ScanState, pkt: &AnyNTPPacket) -> ScanTypeStatus {
 
 pub fn timeout(state: &mut ScanState) -> ScanTypeStatus {
     if state.monlist_request_status.retries < state.maxretries {
-        let mut msg = NtpdPrivatePacket::empty();
-        msg.version = 2;
-        // retry with older implementation
-        msg.implementation = packets::private::IMPL_XNTPD_OLD;
-        msg.reqcode = crate::packets::private::REQ_MON_GETLIST_1;
-        state.queue.push_back(AnyNTPPacket::Private(msg));
-        state.monlist_request_status.retries += 1;
+        init(state);
         ScanTypeStatus::Continue
     } else {
         vprintln!("{} (mode 7) monlist timed out", state.address);
